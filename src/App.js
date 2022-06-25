2d6652ef626185d16177257a2b0545bea50d68ab
sajid-munawar <sajidmunawar666@gmail.com>
2022-06-25 19:30:48 +0500

    Create first component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className='content'>
+        <h1>Component</h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>This is navbar</h1>
+            <a href="/">Home</a>
+            <a href="About">About</a>
+
+        </div>
+     );
+}
+ 
+export default Navbar;
